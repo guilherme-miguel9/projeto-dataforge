2584e1825eb87ee2556c3d02921ab403ebd11fb3
Guilherme Miguel <guicaruaru@hotmail.com>
2026-08-20 11:57:31 -0300

    <update> atualizando o path, agora exibe uma lista para criação das pastas necessárias, facilitando a manutenção do bloco, sem precisar adicionar manualmente na função.

--- src/dataforge/utils/paths.py
+++ src/dataforge/utils/paths.py
@@ -2,15 +2,20 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
 
-DATA_DIR = BASE_DIR / "data"
-RAW_DATA_DIR = DATA_DIR / "raw"
-PROCESSED_DATA_DIR = DATA_DIR / "processed"
 
+DATA_DIR = BASE_DIR / "data"                                                                                          
+RAW_DATA_DIR = DATA_DIR / "raw"                                                                                       
+PROCESSED_DATA_DIR = DATA_DIR / "processed" 
 
+ALL_DIRECTORIES = [
+    DATA_DIR,
+    RAW_DATA_DIR,
+    PROCESSED_DATA_DIR
+]
 
 def init_directories() -> list[Path]:
     list_dir = []
-    for directory in [DATA_DIR, RAW_DATA_DIR, PROCESSED_DATA_DIR]:
+    for directory in ALL_DIRECTORIES:
         directory.mkdir(parents= True, exist_ok = True)
         list_dir.append(directory)
     return list_dir 
