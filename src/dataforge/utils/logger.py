ef34618c6ae3157a217f1b971fd2aa53e380c9b2
Guilherme Miguel <guicaruaru@hotmail.com>
2026-08-24 17:16:31 -0300

    feature: adição do Log de eventos mais detalhado e remoção do Print() como padrão do projeto.

---
+++ src/dataforge/utils/logger.py
@@ -0,0 +1,39 @@
+import logging
+import json
+import sys
+from datetime import datetime
+
+class JsonFormatter(logging.Formatter):
+    def format(self, record: logging.LogRecord) -> str:
+        log_entry = {
+            "timestamp" : datetime.now().isoformat(),
+            "level" : record.levelname,
+            "module" : record.module,
+            "message" : record.getMessage(),
+        }
+        if record.exc_info:
+            log_entry['exception'] = self.formatException(record.exc_info)
+        return json.dumps(log_entry, ensure_ascii=False)
+
+def get_logger(name: str = 'dataforge') -> logging.Logger:
+    logger = logging.getLogger(name)
+
+    if not logger.handlers:
+        logger.setLevel(logging.INFO)
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setFormatter(JsonFormatter())
+        logger.addHandler(handler)
+        logger.propagate = False
+    
+    return logger
+
+if __name__ == "__main__":
+    
+    log = get_logger('teste')
+    log.info('Isso é um log infromativo de sucesso.')
+    log.warning('Aviso de atenção')
+    log.error('Log de erro')
+    log.critical('teste de tcritico')
+
+
+    
